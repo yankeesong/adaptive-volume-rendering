dfb2eaac7dc87cdf53bec46e243e7c622b271ef8
Yanke Song <ysong@g.harvard.edu>
2022-12-05 15:56:47 -0500

    Modified raymarcher coordinate system

--- renderers.py
+++ renderers.py
@@ -222,7 +222,10 @@ class VolumeRenderer(nn.Module):
 
         # Compute the ray directions in world coordinates.
         # Use the function get_world_rays.
-        ros, rds = get_world_rays(x_pix, intrinsics, cam2world)
+        ros, rds = get_world_rays(x_pix, intrinsics, cam2world) # (NV, num_ways, 3)
+
+        z_scales = rds[...,2].unsqueeze(2) # (NV, num_ways, 1)
+
 
         # Generate the points along rays and their depth values
         # Use the function sample_points_along_rays.
@@ -252,11 +255,14 @@ class VolumeRenderer(nn.Module):
         # print(f'rad has shape {rad.shape}')
 
         # Compute pixel colors, depths, and weights via the volume integral.
-        rgb, depth_map, weights = volume_integral(z_vals, sigma, rad)
+        rgb, depth_map, weights = volume_integral(z_vals, sigma, rad) # (NV, num_rays, _)
 
         if self.white_back:
             accum = weights.sum(dim=-2)
             rgb = rgb + (1. - accum)
+        
+        # Re-calculate depth map since rds now does not have z=1
+        depth_map = depth_map * torch.abs(z_scales)
 
         return rgb, depth_map
 
@@ -295,14 +301,16 @@ class Raymarcher(nn.Module):
     def forward(self,
                 cam2world,
                 intrinsics,
-                uv,
-                phi):
-        batch_size, num_samples, _ = uv.shape
+                xy_pix, # xy_pix has shape 
+                phi):  # Here phi is pixelnerf net
+        NV, num_rays, _ = xy_pix.shape
 
-        ray_dirs = get_ray_directions(uv, cam2world=cam2world, intrinsics=intrinsics)
+        #ray_dirs = get_ray_directions(uv, cam2world=cam2world, intrinsics=intrinsics)
+        ros, rds = get_world_rays(xy_pix, intrinsics=intrinsics, cam2world=cam2world) # (NV, num_rays, 3)
 
-        initial_depth = torch.zeros((batch_size, num_samples, 1)).normal_(mean=0.05, std=5e-4).to(uv.device)
-        init_world_coords = world_from_xy_depth(uv, initial_depth, intrinsics=intrinsics, cam2world=cam2world)
+        initial_depth = torch.zeros((NV, num_rays, 1)).normal_(mean=0.8, std=5e-4).to(xy_pix.device)
+
+        init_world_coords = ros + rds * initial_depth
 
         world_coords = [init_world_coords]
         depths = [initial_depth]
@@ -310,29 +318,26 @@ class Raymarcher(nn.Module):
 
 
         for step in range(self.steps):
-            world_coords[-1] = world_coords[-1].reshape(1,-1,3)  # (1, batch_size*num_samples, 3)
-            ray_dirs = ray_dirs.reshape(1,-1,3)
 
-            v = phi(world_coords[-1], viewdirs = ray_dirs, return_features = True)
+            v = phi(world_coords[-1].reshape(1,-1,3), viewdirs = rds.reshape(1,-1,3), return_features = True)   # (1, NV*num_rays, self.n_feature_channels)
 
-            state = self.lstm(v.reshape(-1, self.n_feature_channels), states[-1])
+            state = self.lstm(v.squeeze(0), states[-1])  # (NV*num_rays, self.n_feature_channels)
 
             if state[0].requires_grad:
                 state[0].register_hook(lambda x: x.clamp(min=-10, max=10))
 
-            signed_distance = self.out_layer(state[0]).view(batch_size, num_samples, 1)
-            ray_dirs = ray_dirs.reshape(batch_size, num_samples,3)
-            world_coords[-1] = world_coords[-1].reshape(batch_size, num_samples,3)
-            new_world_coords = world_coords[-1] + ray_dirs * signed_distance
+            signed_distance = self.out_layer(state[0]).view(NV, num_rays, 1) # (NV, num_rays, 1)
+
+            new_world_coords = world_coords[-1] + rds * signed_distance
 
             states.append(state)
             world_coords.append(new_world_coords)
 
             depth = depth_from_world(world_coords[-1], cam2world)
             # commented out for now
-            # if self.training:
-            #     print("Raymarch step %d: Min depth %0.6f, max depth %0.6f" %
-            #           (step, depths[-1].min().detach().cpu().numpy(), depths[-1].max().detach().cpu().numpy()))
+
+            print("Raymarch step %d: Min depth %0.6f, max depth %0.6f" %
+                      (step, depths[-1].min().detach().cpu().numpy(), depths[-1].max().detach().cpu().numpy()))
 
             depths.append(depth)
 
@@ -352,14 +357,11 @@ class Raymarcher(nn.Module):
         #     log.append(('figure', 'stopping_distances', fig, 100))
         self.counter += 1
 
-        world_coords[-1] = world_coords[-1].reshape(1,-1,3)  # (1, batch_size*num_samples, 3)
-        ray_dirs = ray_dirs.reshape(1,-1,3)
-
-        output = phi(world_coords[-1], viewdirs = ray_dirs, return_features = False)
+        output = phi(world_coords[-1].reshape(1,-1,3), viewdirs = rds.reshape(1,-1,3), return_features = False)
 
 
-        rgb = output[..., :3].reshape(batch_size, num_samples,3)
-        sigma = output[..., 3:4].reshape(batch_size, num_samples,1) 
+        rgb = output[..., :3].reshape(NV, num_rays,3)
+        sigma = output[..., 3:4].reshape(NV, num_rays,1) 
         # return world_coords[-1], depths[-1], log
         return rgb, sigma # sigma isn't depth, but should affect loss calculation
 
@@ -520,158 +522,3 @@ class AdaptiveVolumeRenderer(nn.Module):
             # depth_std=conf.get_float("depth_std", 0.01),
             white_back=conf.get_float("white_back", white_back),
         )
-
-
-class OldAdaptiveVolumeRenderer(nn.Module):
-    def __init__(self, near, far, num_feature_channels, raymarch_steps, hidden_size = 16, n_coarse=32, n_fine=16, n_fine_depth=8, depth_std = 0.01, white_back=True):
-        super().__init__()
-        self.near = near
-        self.far = far
-        self.n_coarse = n_coarse
-        self.n_fine = n_fine
-        self.n_fine_depth = n_fine_depth
-        self.depth_std = depth_std
-        self.white_back = white_back
-
-        self.n_feature_channels = num_feature_channels
-        self.steps = raymarch_steps
-
-        # hidden_size = 16
-        self.lstm = nn.LSTMCell(input_size=self.n_feature_channels,
-                                hidden_size=hidden_size)
-
-        self.lstm.apply(init_recurrent_weights)
-        lstm_forget_gate_init(self.lstm)
-
-        self.out_layer = nn.Linear(hidden_size, 1)
-        self.counter = 0
-        
-
-    def forward(
-        self, 
-        phi,
-        cam2world,
-        intrinsics,
-        x_pix,
-        radiance_field: nn.Module
-        ) -> Tuple[torch.tensor, torch.tensor]:
-        """
-        Takes as inputs ray origins and directions - samples points along the 
-        rays and then calculates the volume rendering integral. 
-
-        Params:
-            input_dict: Dictionary with keys 'cam2world', 'intrinsics', and 'x_pix'
-                here x_pix has size (B,2)
-            radiance_field: nn.Module instance of the radiance field we want to render. This should by default the PixelNeRF. 
-                Params: 
-                    xyz: points in space, with shape (SB,B,3)
-                        SB is batch of objects
-                        B is batch of points (in rays)
-                Return:
-                    (SB, B, 4) of r g b sigma
-            phi: FC block if in SRN (NOTE: could also be radiance field here)
-
-        Returns:
-            Tuple of rgb, depth_map
-            rgb: for each pixel coordinate x_pix, the color of the respective ray.
-            depth_map: for each pixel coordinate x_pix, the depth of the respective ray.
- 
-        """
-        batch_size, num_rays = x_pix.shape[0], x_pix.shape[1]  # _ should be 2, since each pixel coordinate has 2 inputs
-
-        log = list()
-
-        ray_dirs = get_ray_directions(x_pix,
-                                               cam2world=cam2world,
-                                               intrinsics=intrinsics) # from geometry (SRN)
-
-        initial_depth = torch.zeros((batch_size, num_rays, 1)).normal_(mean=0.05, std=5e-4).cuda()
-        init_world_coords = world_from_xy_depth(x_pix,
-                                                initial_depth,
-                                                intrinsics=intrinsics,
-                                                cam2world=cam2world)  # from geometry (SRN)
-
-        world_coords = [init_world_coords]
-        depths = [initial_depth]
-        states = [None]
-
-        for step in range(self.steps):
-            v = phi(world_coords[-1]) # could replace by radiance_field instead of phi
-
-            state = self.lstm(v.view(-1, self.n_feature_channels), states[-1])
-
-            if state[0].requires_grad:
-                state[0].register_hook(lambda x: x.clamp(min=-10, max=10))
-
-            signed_distance = self.out_layer(state[0]).view(batch_size, num_rays, 1)
-            new_world_coords = world_coords[-1] + ray_dirs * signed_distance
-
-            states.append(state)
-            world_coords.append(new_world_coords)
-
-            depth = depth_from_world(world_coords[-1], cam2world)  # from geometry (SRN)
-
-            if self.training:
-                print("Raymarch step %d: Min depth %0.6f, max depth %0.6f" %
-                      (step, depths[-1].min().detach().cpu().numpy(), depths[-1].max().detach().cpu().numpy()))
-
-            depths.append(depth)
-
-        if not self.counter % 100:
-            # Write tensorboard summary for each step of ray-marcher.
-            drawing_depths = torch.stack(depths, dim=0)[:, 0, :, :]
-            drawing_depths = lin2img(drawing_depths).repeat(1, 3, 1, 1) # from util (SRN)
-            log.append(('image', 'raycast_progress',
-                        torch.clamp(torchvision.utils.make_grid(drawing_depths, scale_each=False, normalize=True), 0.0,
-                                    5),
-                        100))
-
-            # Visualize residual step distance (i.e., the size of the final step)
-            fig = show_images([lin2img(signed_distance)[i, :, :, :].detach().cpu().numpy().squeeze()
-                                    for i in range(batch_size)]) # from util (SRN)
-            log.append(('figure', 'stopping_distances', fig, 100))
-        self.counter += 1
-
-        # Compute the ray directions in world coordinates.
-        # Use the function get_world_rays.
-        ros, rds = get_world_rays(x_pix, intrinsics, cam2world)
-
-        # Generate the points along rays and their depth values
-        # Use the function sample_points_along_rays.
-
-        pts, z_vals = sample_points_along_rays(depth[-1].min(), depth[-1].max(), self.n_coarse, 
-                                                ros, rds, device=x_pix.device) # might not be right.
-
-        # Reshape pts to (batch_size, -1, 3).
-        pts = pts.reshape(batch_size, -1,3)  # To comply with pixelnerf input
-
-        # Sample the radiance field with the points along the rays.
-        sigma_rad = radiance_field(pts)
-        sigma_rad = sigma_rad.squeeze(0)   # Get rid of SB dimension
-        sigma = sigma_rad[...,3]
-        rad = sigma_rad[...,:3]
-
-
-        # Reshape sigma and rad back to (batch_size, num_rays, self.n_samples, -1)
-        sigma = sigma.view(batch_size, num_rays, self.n_coarse, 1)
-        rad = rad.view(batch_size, num_rays, self.n_coarse, 3)
-
-        # Compute pixel colors, depths, and weights via the volume integral.
-        rgb, depth_map, weights = volume_integral(z_vals, sigma, rad)
-
-        if self.white_back:
-            accum = weights.sum(dim=-2)
-            rgb = rgb + (1. - accum)
-
-        return log, rgb, depth_map # world_coords[-1], depths[-1] (might not need)
-
-    @classmethod
-    def from_conf(cls, conf, white_back=False):
-        return cls(
-            conf.get_int("n_coarse", 32),
-            conf.get_int("n_fine", 16),
-            n_fine_depth=conf.get_int("n_fine_depth", 8),
-            depth_std=conf.get_float("depth_std", 0.01),
-            white_back=conf.get_float("white_back", white_back),
-        )
-    
