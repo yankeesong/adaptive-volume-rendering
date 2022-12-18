d5e555a90d1e15f93d2990b4030602cc14f75c56
Yanke Song <ysong@g.harvard.edu>
2022-12-19 04:33:26 +0800

    clean up

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # adaptive-volume-rendering
+
+## What pixelnerf is doing
+- SB is 4, NV is 
+- For non-visualization steps, only sample a batch of 128 rays
+- Loss function for both coarse and fine images, equal weights
+- Fine sampling: fine-depth samples, importance sampling
+- Depth sampling: depth samples, random sampling
+- Collect all samples, sort, then pass through fine network
+
+## TODO list
+- Rewrite the dataloader so we have train, val, test
+- Distinguish between nn.train and nn.eval
+- Allow superbatch
+- Rewrite the trainer so we are sampling a subset of rays, but include all views
+- Rewrite the trainder so we we have visualization step and validation step
+- Do coarse + fine sampling
+- For visualization, only do fine
--- dataset.py
+++ dataset.py
@@ -1,122 +1,5 @@
 from utils import *
 
-# def parse_rgb(hdf5_dataset):
-#     s = hdf5_dataset[...].tobytes()
-#     f = io.BytesIO(s)
-
-#     img = imageio.imread(f)[:, :, :3]
-#     img = skimage.img_as_float32(img)
-#     return img
-
-
-# def parse_intrinsics(hdf5_dataset):
-#     s = hdf5_dataset[...].tobytes()
-#     s = s.decode('utf-8')
-
-#     lines = s.split('\n')
-#     f, cx, cy, _ = map(float, lines[0].split())
-#     full_intrinsic = torch.tensor([[f, 0., cx],
-#                                     [0., f, cy],
-#                                     [0., 0, 1]])
-
-#     return full_intrinsic
-
-
-# def parse_pose(hdf5_dataset):
-#     raw = hdf5_dataset[...]
-#     ba = bytearray(raw)
-#     s = ba.decode('ascii')
-
-#     lines = s.splitlines()
-#     pose = np.zeros((4, 4), dtype=np.float32)
-
-#     for i in range(16):
-#         pose[i // 4, i % 4] = lines[0].split(" ")[i]
-    
-#     pose = torch.from_numpy(pose.squeeze())
-#     pose = pose @ torch.diag(
-#             torch.tensor([1, -1, -1, 1], dtype=torch.float32))
-#     return pose
-    
-
-# class SRNsCars(IterableDataset):
-#     def __init__(self, working_dir, max_num_instances=None, img_sidelength=None):
-#         self.f = h5py.File(f'{working_dir}data/cars_train.hdf5', 'r')
-#         self.instances = sorted(list(self.f.keys()))
-
-#         self.img_sidelength = img_sidelength
-
-#         if max_num_instances:
-#             self.instances = self.instances[:max_num_instances]
-
-#     def __len__(self):
-#         return len(self.instances)
-
-#     def __iter__(self, override_idx=None):
-#         while True:
-#             if override_idx is not None:
-#                 idx = override_idx
-#             else:
-#                 idx = random.randint(0, len(self.instances)-1)
-                
-#             key = self.instances[idx]
-            
-#             instance = self.f[key]
-#             rgbs_ds = instance['rgb']
-#             c2ws_ds = instance['pose']
-
-#             rgb_keys = list(rgbs_ds.keys())
-#             c2w_keys = list(c2ws_ds.keys())
-
-#             observation_idx = np.random.randint(0, len(rgb_keys))
-#             rgb = parse_rgb( rgbs_ds[rgb_keys[observation_idx]] )
-
-#             x_pix = get_opencv_pixel_coordinates(*rgb.shape[:2])
-
-#             # There is a lot of white-space around the cars - we'll thus crop the images a bit:
-#             # rgb = rgb[32:-32, 32:-32]
-#             # x_pix = x_pix[32:-32, 32:-32]
-
-#             # Nearest-neighbor downsampling of *both* the
-#             # RGB image and the pixel coordinates. This is better than down-
-#             # sampling RGB only and then generating corresponding pixel coordinates,
-#             # which generates "fake rays", i.e., rays that the camera
-#             # didn't actually capture with wrong colors. Instead, this simply picks a 
-#             # subset of the "true" camera rays.
-#             if self.img_sidelength is not None and rgb.shape[0] != self.img_sidelength:
-#                 rgb = resize(rgb, 
-#                              (self.img_sidelength, self.img_sidelength), 
-#                              anti_aliasing=False,
-#                              order=0)
-#                 rgb = torch.from_numpy(rgb)
-#                 x_pix = resize(x_pix, 
-#                                (self.img_sidelength, self.img_sidelength), 
-#                                anti_aliasing=False,
-#                                order=0)
-                
-#             x_pix = rearrange(x_pix, 'i j c -> (i j) c')
-#             c2w = parse_pose( c2ws_ds[c2w_keys[observation_idx]] )
-
-#             rgb = rearrange(rgb, 'i j c -> (i j) c')
-
-#             # print(f'key is {key}')
-#             # print(f'observation idx is {observation_idx}')
-#             # print(f'pose key is {c2w_keys[observation_idx]}')
-
-#             intrinsics = parse_intrinsics( instance['intrinsics.txt'] )
-#             intrinsics[:2, :3] /= 128. # Normalize intrinsics from resolution-specific intrinsics for 128x128
-
-#             model_input = {"cam2world": c2w,
-#                            "intrinsics": intrinsics,
-#                            "focal": intrinsics[0,0]*self.img_sidelength, # For pixelnerf
-#                            "c":torch.tensor([intrinsics[0,2]*self.img_sidelength, intrinsics[1,2]*self.img_sidelength], dtype=torch.float32), # For pixelnerf
-#                            "x_pix": x_pix, # For volume renderer
-#                            "idx": torch.tensor([idx]),
-#                            "images": rgb}
-
-#             yield model_input
-
-
 def pick(list, item_idcs):
     if not list:
         return list
@@ -207,17 +90,7 @@ class SceneInstanceDataset():
             self.color_paths = pick(self.color_paths, specific_observation_idcs)
             self.pose_paths = pick(self.pose_paths, specific_observation_idcs)
             self.param_paths = pick(self.param_paths, specific_observation_idcs)
-        # Filter out bad images
-        idcs = []
-        for i in range(len(self.pose_paths)):
-            if torch.from_numpy(load_pose(self.pose_paths[i]))[2,3]>0:
-                idcs.append(i)
-#         print(f'good {len(idcs)} poses')
-#         print(f'total {len(self.color_paths)} colors')
-#         print(instance_dir)
-        self.color_paths = pick(self.color_paths, idcs)
-        self.pose_paths = pick(self.pose_paths, idcs)
-        self.param_paths = pick(self.param_paths, idcs)
+
         if specific_observation_idcs is None and num_images != -1:
             idcs = np.linspace(0, stop=len(self.color_paths), num=num_images, endpoint=False, dtype=int)
             self.color_paths = pick(self.color_paths, idcs)
@@ -241,8 +114,6 @@ class SceneInstanceDataset():
 
         x_pix = get_opencv_pixel_coordinates(*rgb.shape[:2])
 
-        # rgb = rgb[32:-32, 32:-32]
-        # x_pix = x_pix[32:-32, 32:-32]
 
         if self.img_sidelength is not None and rgb.shape[0] != self.img_sidelength:
             rgb = resize(rgb, 
